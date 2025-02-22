53905e1b76ddfc6d6c92752a269db58cbc67903d
MAHMUDHASANSADI <mhasan181075@bscse.uiu.ac.bd>
2025-02-22 11:48:42 +0600

    templating complete

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layouts')
+@section('content')
+    <div class="containter">
+        <h1>Terms</h1>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque reprehenderit ex odit consequatur repudiandae minima perferendis sit possimus eius tenetur aut, voluptate repellendus harum. Error laboriosam voluptatem eum enim alias.
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+Route::get('/terms', function () {
+    return view('terms');
 });
